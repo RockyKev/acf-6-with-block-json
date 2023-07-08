7247879894eaaaab0989a3171cbf3b2ecbf31e3b
rkevwsl <kev.rocky@gmail.com>
2023-07-08 00:34:53 -0700

    Added all the blocks

---
+++ blocks/acf-advanced/google-map/template.php
@@ -0,0 +1,19 @@
+<?php 
+
+// Text Field
+// https://www.advancedcustomfields.com/resources/google-map/
+
+$email = esc_html(get_field('email'));
+
+$additionalClasses = !empty($block['className']) ? $block['className'] : 'no-classes-added';
+?>
+
+<div class="outline outline-4 outline-blue-500 py-4 my-4 <?= $additionalClasses; ?>">
+
+    <h1 class="text-4xl underline pb-4">ACF-basic/Email</h1>
+
+    <p>Test email: <?= $email; ?></p>
+
+</div>
+
+
--- functions.php
+++ functions.php
@@ -30,7 +30,40 @@ register_block_type( get_template_directory() . '/blocks/acf-basic/text/block.js
 register_block_type( get_template_directory() . '/blocks/acf-basic/text-area/block.json' );
 register_block_type( get_template_directory() . '/blocks/acf-basic/url/block.json' );
 
+register_block_type( get_template_directory() . '/blocks/acf-advanced/color-picker/block.json' );
+register_block_type( get_template_directory() . '/blocks/acf-advanced/date-picker/block.json' );
+register_block_type( get_template_directory() . '/blocks/acf-advanced/date-time-picker/block.json' );
+register_block_type( get_template_directory() . '/blocks/acf-advanced/google-map/block.json' );
+register_block_type( get_template_directory() . '/blocks/acf-advanced/time-picker/block.json' );
 
+
+register_block_type( get_template_directory() . '/blocks/acf-choice/button-group-with-text/block.json' );
+register_block_type( get_template_directory() . '/blocks/acf-choice/checkbox-with-text/block.json' );
+register_block_type( get_template_directory() . '/blocks/acf-choice/radio-button-with-text/block.json' );
+register_block_type( get_template_directory() . '/blocks/acf-choice/select-with-text/block.json' );
+register_block_type( get_template_directory() . '/blocks/acf-choice/true-false-with-text/block.json' );
+
+register_block_type( get_template_directory() . '/blocks/acf-content/file/block.json' );
+register_block_type( get_template_directory() . '/blocks/acf-content/image/block.json' );
+register_block_type( get_template_directory() . '/blocks/acf-content/oEmbed/block.json' );
+register_block_type( get_template_directory() . '/blocks/acf-content/pro-gallery/block.json' );
+register_block_type( get_template_directory() . '/blocks/acf-content/wysiwyg/block.json' );
+
+register_block_type( get_template_directory() . '/blocks/acf-layout/accordion-with-text/block.json' );
+register_block_type( get_template_directory() . '/blocks/acf-layout/group-with-text/block.json' );
+register_block_type( get_template_directory() . '/blocks/acf-layout/message/block.json' );
+register_block_type( get_template_directory() . '/blocks/acf-layout/pro-clone/block.json' );
+register_block_type( get_template_directory() . '/blocks/acf-layout/pro-flexible-content/block.json' );
+register_block_type( get_template_directory() . '/blocks/acf-layout/pro-repeater-with-wysiwyg/block.json' );
+register_block_type( get_template_directory() . '/blocks/acf-layout/tab-with-wysiwyg/block.json' );
+
+
+register_block_type( get_template_directory() . '/blocks/acf-relational/link/block.json' );
+register_block_type( get_template_directory() . '/blocks/acf-relational/page-link/block.json' );
+register_block_type( get_template_directory() . '/blocks/acf-relational/post-object/block.json' );
+register_block_type( get_template_directory() . '/blocks/acf-relational/relationship/block.json' );
+register_block_type( get_template_directory() . '/blocks/acf-relational/taxonomy/block.json' );
+register_block_type( get_template_directory() . '/blocks/acf-relational/user/block.json' );
 
 // TODO: create function that loops through all the blocks.
 // https://developer.wordpress.org/reference/functions/register_block_type/#comment-6248
